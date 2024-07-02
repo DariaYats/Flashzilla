1a70179c169b555638dc5d71621e77e35b754cdb
Daria Yats <dariayatsyniuk@gmail.com>
2024-07-02 16:13:51 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Дарья Яцынюк on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
