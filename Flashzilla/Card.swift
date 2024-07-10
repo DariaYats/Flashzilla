74ee9d91ae9c0581b2af25638f4c2cbbc469c8f3
Daria Yats <dariayatsyniuk@gmail.com>
2024-07-10 16:04:14 +0200

    create a stack of flashcards which user could drag to the both side

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Дарья Яцынюк on 08.07.2024.
+//
+
+import Foundation
+
+
+struct Card {
+    var promt: String
+    var answer: String
+    
+    static let example = Card(promt: "Who played special FBI agent in Twin Peaks", answer: "Kyle Maclachlan")
+}
